6d2122bf29dcd1c955b44eb1dfd3f6b38fd0e13b
Sushil97 <sushil500530@gmail.com>
2023-09-28 19:19:57 +0600

    add to email,home,login,header,navbar,and other components stuctured

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+    return (
+        <div>
+            <h2 className="text-2xl text-center">Please login</h2>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Resister/Resister.jsx
@@ -0,0 +1,10 @@
+
+const Resister = () => {
+    return (
+        <div>
+            <h2 className="text-2xl text-center">Please Resister</h2>
+        </div>
+    );
+};
+
+export default Resister;
