398f0b592daabb3462a48bf4d8ff076c1d1268f9
Sushil97 <sushil500530@gmail.com>
2023-09-29 07:37:51 +0600

    add more validation in input field

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,76 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/Firebase";
+import { useState } from "react";
 
 const Login = () => {
+
+    const [loginError, setLoginError] = useState('');
+    const [loginSuccess, setLoginSuccess] = useState('')
+    const handleLogin = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        setLoginError('');
+        setLoginSuccess('')
+        console.log(email, password);
+
+        if (!password) {
+            setLoginError("invalid password")
+            return;
+        }
+        // add validation
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user);
+                setLoginSuccess("User Created Successfully.")
+            })
+            .catch(error => {
+                console.error(error);
+                setLoginError('Invalid Password!')
+                // setLoginError("Invalid Password")
+            })
+    }
+    console.log(loginError);
     return (
         <div>
-            <h2 className="text-2xl text-center">Please login</h2>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <form onSubmit={handleLogin}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" name="email" required placeholder="email" className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" name="password" required placeholder="password" className="input input-bordered" />
+                                    <label className="label">
+                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    </label>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Login</button>
+                                </div>
+                            </form>
+                            {
+                                loginError && <p className="text-red-500">{loginError}</p>
+                            }
+                            {
+                                loginSuccess && <p className="text-green-500">{loginSuccess}</p>
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Resister/Resister.jsx
+++ src/components/Resister/Resister.jsx
@@ -1,17 +1,19 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/Firebase";
 import { useState } from "react";
-import {FaEye,FaEyeSlash } from 'react-icons/fa';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Resister = () => {
     const [errorCatch, setErrorCatch] = useState('');
     const [successShow, setSuccessShow] = useState('');
-    const [showPassword,setShowPassword] = useState(false) ;
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleResister = (e) => {
         e.preventDefault()
         const email = e.target.email.value;
         const password = e.target.password.value;
+        const acceptedTerms = e.target.terms.checked ;
+        console.log(acceptedTerms);
         // reset error
         setErrorCatch('');
         setSuccessShow('');
@@ -23,7 +25,10 @@ const Resister = () => {
             setErrorCatch("your password should have at least one uppercase characters.")
             return;
         }
-
+        else if(!acceptedTerms){
+            setErrorCatch("Please accept our terms and conditions!")
+            return ;
+        }
         // console.log(email,password);
         // create user 
         createUserWithEmailAndPassword(auth, email, password)
@@ -35,28 +40,34 @@ const Resister = () => {
                 setErrorCatch(error.message)
             })
     }
-    console.log(errorCatch);
-    console.log(successShow);
+    // console.log(errorCatch);
+    // console.log(successShow);
     return (
         <div>
             <div className="bg-gray-400 w-1/2 mx-auto p-6 mt-5">
                 <h2 className="text-3xl font-bold text-center">Please Resister</h2>
-                <form onSubmit={handleResister} className="flex justify-center items-center flex-col mt-6">
+                <form onSubmit={handleResister} className="flex flex-col mt-6">
                     <input required type="email" placeholder="Email" name="email" className="input input-bordered input-info w-full block" /> <br></br>
-                  
-                   <div className="flex relative w-full ">
-                   <input type={showPassword ? "text" : "password"}
-                    placeholder="Password" 
-                    name="password" required 
-                    className="input input-bordered input-info block w-full" />
-                    <span onClick={()=>setShowPassword(!showPassword)} className="absolute top-0 right-0 p-3 text-xl font-medium cursor-pointer">
-                    {
-                        showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>
-                    }
-                    </span>
-                   </div>
-                    
-                    <button className="btn bg-green-500 text-xl font-medium text-white hover:bg-transparent hover:border-green-500 hover:text-green-800 transition mt-5 ease-in">Resister</button>
+
+                    <div className="flex relative w-full ">
+                        <input type={showPassword ? "text" : "password"}
+                            placeholder="Password"
+                            name="password" required
+                            className="input input-bordered input-info block w-full" />
+                        <span onClick={() => setShowPassword(!showPassword)} className="absolute top-0 right-0 p-3 text-xl font-medium cursor-pointer">
+                            {
+                                showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>
+                            }
+                        </span>
+                    </div>
+                    <div className="space-x-3">
+                        <input type="checkbox" name="terms" id="terms" className="mt-3 text-start text-xl" />
+                        <label htmlFor="terms">Accept Our <a href="#" className="text-blue-600 hover:underline"> Terms and Conditions</a></label>
+                    </div>
+                    <div className="flex items-center justify-center mx-auto">
+                        <button className="btn bg-green-500 text-xl font-medium text-white hover:bg-transparent hover:border-green-500 hover:text-green-800 transition mt-5 ease-in">Resister</button>
+                    </div>
+
                 </form>
                 {
                     errorCatch && <p className="text-xl text-red-600">{errorCatch}</p>
